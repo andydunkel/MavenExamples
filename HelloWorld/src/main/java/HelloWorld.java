e09009dcd81eb65b1ebd8447acf370ab5aceb5aa
Andy Dunkel <a.dunkel@pilz.de>
2021-12-02 15:23:18 +0100

    pom.xml überarbeitet

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class HelloWorld {
     public static void main(String arg[]) {
-        System.out.println("Hello World");
+        var test = NumberUtils.toInt("100");
+        System.out.println("Hello World: " + test);
     }
 }
